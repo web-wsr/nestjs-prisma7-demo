a9b14dd5ea70f5199a72b7b95309eb1d8cb8a6f1
web-wsr <2316516412@qq.com>
2026-06-26 00:32:45 +0800

    Add Chains module and extend Prompts API

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { PostModule } from './post/post.module';
 import { ModelsModule } from './models/models.module';
 import { PromptsModule } from './prompts/prompts.module';
+import { ChainsModule } from './chains/chains.module';
 
 @Module({
-  imports: [OrderModule, UserModule, PrismaModule, PostModule, ModelsModule, PromptsModule],
+  imports: [OrderModule, UserModule, PrismaModule, PostModule, ModelsModule, PromptsModule, ChainsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/prompts/prompts.controller.ts
+++ src/prompts/prompts.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { PromptsService } from './prompts.service';
 
 @Controller('prompts')
-export class PromptsController {}
+export class PromptsController {
+  constructor(private readonly promptsService: PromptsService) {}
+
+  @Post('translate')
+  translate(@Body() body: { text: string; targetLanguage: string }) {
+    return this.promptsService.translate(body.text, body.targetLanguage);
+  }
+  @Post('summarize')
+  summarize(@Body() body: { text: string; maxWords: number }) {
+    return this.promptsService.summarizeText(body.text, body.maxWords);
+  }
+  @Post('classify')
+  classify(@Body() body: { text: string }) {
+    return this.promptsService.classifyText(body.text);
+  }
+  @Post('codeReview')
+  codeReview(@Body() body: { code: string; language: string }) {
+    return this.promptsService.codeReview(body.code, body.language);
+  }
+}
